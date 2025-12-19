33bcd66838118cce13624b7c7e8bf357f1e076f9
Aviral00Jain <jainaviral183@gmail.com>
2025-12-19 16:14:35 +0530

    [Aviral] : added the vote Eligibility

---
+++ gcr-codebase/arrays/Level-1/VotingEligibility.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Define array for 10 students' ages
+        int[] ages = new int[10];
+
+        // Take user input for ages
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            ages[i] = input.nextInt();
+        }
+
+        System.out.println();
+
+        // Check voting eligibility
+        for (int i = 0; i < ages.length; i++) {
+
+            if (ages[i] < 0) {
+                System.out.println("Invalid age: " + ages[i]);
+            }
+            else if (ages[i] >= 18) {
+                System.out.println("The student with the age " + ages[i] + " can vote.");
+            }
+            else {
+                System.out.println("The student with the age " + ages[i] + " cannot vote.");
+            }
+        }
+
+        input.close();
+    }
+}
