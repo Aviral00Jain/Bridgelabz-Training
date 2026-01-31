56edb8cc53dc45a6d88b9fcdd5d84c4b7e26a13c
Aviral00Jain <jainaviral183@gmail.com>
2026-01-31 15:54:39 +0530

    [Aviral]: Added a program of csv files

---
+++ java-iostreams-practice/gcr-codebase/java-iostrams-practice/src/main/java/IOStreampractice/java_iostrams_practice/App.java
@@ -0,0 +1,10 @@
+package IOStreampractice.java_iostrams_practice;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
