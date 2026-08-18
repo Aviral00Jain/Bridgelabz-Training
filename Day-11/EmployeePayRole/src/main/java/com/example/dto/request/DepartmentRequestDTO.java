6440f0334b0c070ac715350a83aba14ff5362fb2
Aviral00Jain <jainaviral183@gmail.com>
2026-08-18 14:33:15 +0530

    [Aviral]: updated dto file

--- Day-11/EmployeePayRole/src/main/java/com/example/dto/request/DepartmentRequestDTO.java
+++ Day-11/EmployeePayRole/src/main/java/com/example/dto/request/DepartmentRequestDTO.java
@@ -1,7 +1,10 @@
 package com.example.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class DepartmentRequestDTO {
 
+    @NotBlank(message = "Department name is required")
     private String departmentName;
 
     public DepartmentRequestDTO() {
