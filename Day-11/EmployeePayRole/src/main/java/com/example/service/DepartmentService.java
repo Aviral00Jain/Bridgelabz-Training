3b28c8e328d980fd5fb4c0be0f5b57b3561bbf08
Aviral00Jain <jainaviral183@gmail.com>
2026-08-18 14:37:37 +0530

    [Aviral]: updated service class

--- Day-11/EmployeePayRole/src/main/java/com/example/service/DepartmentService.java
+++ Day-11/EmployeePayRole/src/main/java/com/example/service/DepartmentService.java
@@ -1,70 +1,22 @@
 package com.example.service;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
-import com.example.entity.Department;
-import com.example.exception.DepartmentNotFoundException;
-import com.example.repository.DepartmentRepository;
+import com.example.dto.request.DepartmentRequestDTO;
+import com.example.dto.response.DepartmentResponseDTO;
 
-@Service
-public class DepartmentService {
+public interface DepartmentService {
 
-    private final DepartmentRepository repository;
+    DepartmentResponseDTO save(DepartmentRequestDTO request);
 
-    public DepartmentService(DepartmentRepository repository) {
-        this.repository = repository;
-    }
+    Page<DepartmentResponseDTO> findAll(Pageable pageable);
 
-    // CREATE
-    public Department save(Department department) {
-        return repository.save(department);
-    }
+    DepartmentResponseDTO findById(Long id);
 
-    // READ ALL
-    public List<Department> findAll() {
-        return repository.findAll();
-    }
-
-    // READ BY ID
-    public Department findById(Long id) {
-
-        return repository.findById(id)
-                .orElseThrow(() ->
-                        new DepartmentNotFoundException(
-                                "Department not found with id: " + id));
-    }
-
-    // UPDATE
-    public Department update(
+    DepartmentResponseDTO update(
             Long id,
-            Department department) {
+            DepartmentRequestDTO request);
 
-        Department existingDepartment = findById(id);
-
-        existingDepartment.setDepartmentName(
-                department.getDepartmentName());
-
-        return repository.save(existingDepartment);
-    }
-
-    // DELETE
-    public void delete(Long id) {
-
-        if (!repository.existsById(id)) {
-            throw new DepartmentNotFoundException(
-                    "Department not found with id: " + id);
-        }
-
-        repository.deleteById(id);
-    }
-
-    // PAGINATION + SORTING
-    public Page<Department> findAll(Pageable pageable) {
-
-        return repository.findAll(pageable);
-    }
+    void delete(Long id);
 }
