6fca3a02b09d7e387881f1557bcbbdd0a92ac7fb
Aviral00Jain <jainaviral183@gmail.com>
2026-08-20 23:02:11 +0530

    [Aviral]: added a security config file

--- Day-13/FundooNotesApp/src/main/java/com/fundoonotes/config/SecurityConfig.java
+++ Day-13/FundooNotesApp/src/main/java/com/fundoonotes/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.fundoonotes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -10,33 +11,31 @@ import com.fundoonotes.security.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+	
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+	    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
+	
 
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		http.csrf(csrf -> csrf.disable())
+		    .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		    .authorizeHttpRequests(auth -> auth
+		    		.requestMatchers("/auth/register",
+		    						 "/auth/login",
+		    						 "/auth/forgot-password",
+		    						 "/auth/reset-password"
+		    		).permitAll()
+					.anyRequest()
+					.authenticated()
+		    	)
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http
-            .csrf(csrf -> csrf.disable())
-
-            .formLogin(form -> form.disable())
-
-            .httpBasic(basic -> basic.disable())
-
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**").permitAll()
-                .anyRequest().authenticated()
-            )
-
-            .addFilterBefore(
-                jwtAuthenticationFilter,
-                UsernamePasswordAuthenticationFilter.class
-            );
-
-        return http.build();
-    }
+            .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class);
+		
+		return http.build();
+	}
 }
